7da968747db1b61c942e46dbceba34b06f82f787
Guilherme Silva <guilherme.francais@gmail.com>
2021-05-28 02:00:27 -0300

    Add first stable version of jupyter widget

--- turtle-widget/js/lib/example.js
+++ turtle-widget/js/lib/example.js
@@ -1,29 +1,11 @@
 var widgets = require('@jupyter-widgets/base');
 var _ = require('lodash');
-var turtle = require('../node_modules/turtle-component');
+var t = require('../node_modules/turtle-component');
 
-// See example.py for the kernel counterpart to this file.
-
-
-// Custom Model. Custom widgets models must at least provide default values
-// for model attributes, including
-//
-//  - `_view_name`
-//  - `_view_module`
-//  - `_view_module_version`
-//
-//  - `_model_name`
-//  - `_model_module`
-//  - `_model_module_version`
-//
-//  when different from the base class.
-
-// When serialiazing the entire widget state for embedding, only values that
-// differ from the defaults will be specified.
-var TurtleModel = widgets.DOMWidgetModel.extend({
+var TurtleCanvasModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
-        _model_name : 'TurtleModel',
-        _view_name : 'TurtleView',
+        _model_name : 'TurtleCanvasModel',
+        _view_name : 'TurtleCanvasView',
         _model_module : 'turtle-widget',
         _view_module : 'turtle-widget',
         _model_module_version : '0.1.0',
@@ -31,84 +13,77 @@ var TurtleModel = widgets.DOMWidgetModel.extend({
     })
 });
 
-
-// Custom View. Renders the widget model.
-var TurtleView = widgets.DOMWidgetView.extend({
-    // Defines how the widget gets rendered into the DOM
+var TurtleCanvasView = widgets.DOMWidgetView.extend({
     render: function() {
+        this.turtles = {};
         this.create_canvas();
-        this.turtle = this.canvas.getTurtle();
-
-        this.set_turtle();
 
         this.model.on('change:command', this.execute, this);
+        this.model.on('change:last_turtle', this.create_turtle, this);
+    },
+
+    create_turtle: function() {
+        let turtle = this.model.get('last_turtle');
+
+        if (Number.isInteger(turtle.id)) this.turtles[turtle.id] = this.canvas.getTurtle();
     },
 
     create_canvas: function() {
-        // console.log('create canvas');
-
-        this.canvas = new turtle.TurtleComponent(this.el);
+        this.canvas = new t.TurtleComponent();
         this.canvas.width = 400;
         this.canvas.height = 400;
-        this.canvas.canvasStyle = 'border: solid 1px black; position: absolute !important;';
+        this.canvas.canvasStyle = 'border: solid 1px black; position: relative !important;';
+        this.canvas.spriteScale = 0.2;
         this.canvas.initializeCanvas(this.el);
 
         this.set_canvas();
     },
 
-    execute: function() {
-        // console.log('execute canvas');
-        // console.log(this.model.get('command'));
-        this.command = this.model.get('command');
-
-        if (this.command.function_name) {
-            // console.log(this.command.function_name);
-            this.run();
-            this.model.set('command', {});
-            this.model.save_changes();
-        }
-    },
-
     set_canvas: function() {
-        // console.log('set_canvas');
         this.model.set('canvas', { 'element': this.canvas });
         this.model.save_changes();
     },
 
     set_turtle: function() {
-        // console.log('set_turtle');
         this.model.set('turtle', { 'element': this.turtle });
         this.model.save_changes();
     },
 
     get_canvas: function() {
-        // console.log('get_canvas');
         let canvas = this.model.get('canvas');
 
         if (canvas) return canvas.element;
     },
 
     get_turtle: function() {
-        // console.log('get_turtle');
         let turtle = this.model.get('turtle');
 
         if (turtle) return turtle.element;
     },
 
-    run: function() {
-        // console.log('run forward');
+    execute: function () {
+        this.command = this.model.get('command');
 
-        if (this.turtle) {
-            this.turtle[this.command['function_name']](...this.command['args']);
-            this.set_turtle();
+        if (this.command.function_name) {
+            this.run();
+            this.model.set('command', {});
             this.model.save_changes();
         }
     },
 
+    run: function () {
+        let turtle_id = this.command.turtle_id;
+        let turtle = this.turtles[turtle_id];
+
+        if (turtle) {
+            turtle[this.command['function_name']](...this.command['args']);
+            this.set_turtle();
+            this.model.save_changes();
+        }
+    },
 });
 
-
 module.exports = {
-    TurtleModel: TurtleModel,
-    TurtleView: TurtleView
+    TurtleCanvasModel: TurtleCanvasModel,
+    TurtleCanvasView: TurtleCanvasView
 };
--- turtle-widget/turtle_widget/example.py
+++ turtle-widget/turtle_widget/example.py
@@ -1,58 +1,98 @@
 import ipywidgets as widgets
-from traitlets import Unicode, Dict, observe
-
-# See js/lib/example.js for the frontend counterpart to this file.
+from traitlets import Unicode, Dict, Integer, observe
 
 @widgets.register
 class Turtle(widgets.DOMWidget):
-    # Name of the widget view class in front-end
-    _view_name = Unicode('TurtleView').tag(sync=True)
+    canvas = Dict({ 'instance': None }).tag(sync=True)
 
-    # Name of the widget model class in front-end
-    _model_name = Unicode('TurtleModel').tag(sync=True)
-
-    # Name of the front-end module containing widget view
-    _view_module = Unicode('turtle-widget').tag(sync=True)
-
-    # Name of the front-end module containing widget model
-    _model_module = Unicode('turtle-widget').tag(sync=True)
-
-    # Version of the front-end module containing widget view
-    _view_module_version = Unicode('^0.1.0').tag(sync=True)
-    # Version of the front-end module containing widget model
-    _model_module_version = Unicode('^0.1.0').tag(sync=True)
-
-    command = Dict({ 'function_name': 'idle', 'args': [] }).tag(sync=True)
-    canvas = Dict({ 'element': None })
-    turtle = Dict({ 'element': None })
+    def __init__(self, id, canvas):
+        self._model_id = id
+        self.canvas = canvas
+        super()
 
     def forward(self, distance):
-        self.command = { 'function_name': 'forward', 'args': [distance] }
+        self.canvas['instance'].command = {
+            'turtle_id': self._model_id,
+            'function_name': 'forward',
+            'args': [distance]
+        }
 
     def backward(self, distance):
         self.forward(distance*-1)
 
     def right(self, angle):
-        self.command = { 'function_name': 'right', 'args': [angle] }
+        self.canvas['instance'].command = {
+            'turtle_id': self._model_id,
+            'function_name': 'right',
+            'args': [angle]
+        }
 
     def left(self, angle):
-        self.command = { 'function_name': 'left', 'args': [angle] }
+        self.canvas['instance'].command = {
+            'turtle_id': self._model_id,
+            'function_name': 'left',
+            'args': [angle]
+        }
 
     def color(self, color):
-        self.command = { 'function_name': 'setLineColor', 'args': [color] }
+        self.canvas['instance'].command = {
+            'turtle_id': self._model_id,
+            'function_name': 'setLineColor',
+            'args': [color]
+        }
 
     def circle(self, radius):
-        self.command = { 'function_name': 'circle', 'args': [radius] }
+        self.canvas['instance'].command = {
+            'turtle_id': self._model_id,
+            'function_name': 'circle',
+            'args': [radius]
+        }
 
     def rectangle(self, width, height):
-        self.command = { 'function_name': 'rectangle', 'args': [width, height] }
+        self.canvas['instance'].command = {
+            'turtle_id': self._model_id,
+            'function_name': 'rectangle',
+            'args': [width, height]
+        }
 
     def speed(self, value):
-        self.command = { 'function_name': 'speed', 'args': [value] }
+        self.canvas['instance'].command = {
+            'turtle_id': self._model_id,
+            'function_name': 'speed',
+            'args': [value]
+        }
 
     def set_position(self, x, y):
-        self.command = { 'function_name': 'setPosition', 'args': [x, y] }
+        self.canvas['instance'].command = {
+            'turtle_id': self._model_id,
+            'function_name': 'setPosition',
+            'args': [x, y]
+        }
+
+@widgets.register
+class TurtleCanvas(widgets.DOMWidget):
+    _view_name = Unicode('TurtleCanvasView').tag(sync=True)
+    _model_name = Unicode('TurtleCanvasModel').tag(sync=True)
+    _view_module = Unicode('turtle-widget').tag(sync=True)
+    _model_module = Unicode('turtle-widget').tag(sync=True)
+    _view_module_version = Unicode('^0.1.0').tag(sync=True)
+    _model_module_version = Unicode('^0.1.0').tag(sync=True)
+    _auto_increment_counter = Integer(0).tag(sync=True)
+
+    command = Dict({}).tag(sync=True)
+    last_turtle = Dict({}).tag(sync=True)
+
+    @observe('last_turtle')
+    def build_turtles(self, event):
+        self.last_turtle = {}
+
+    def create_turtle(self):
+        turtle = Turtle(self._auto_increment_counter, { 'instance': self })
+        self.last_turtle = { 'id': turtle._model_id }
+        self._auto_increment_counter += 1
+
+        return turtle
 
     @observe('command')
-    def reset_command(self, args):
+    def reset_command(self, event):
         self.command = {}
